d02dab2691180c5936caf1f4be0c60a712f323aa
Michael <mgoudreault1999@gmail.com>
2025-02-21 01:30:37 -0500

    build

---
+++ build/fxmanifest.lua
@@ -0,0 +1,16 @@
+﻿-- CFX Details
+lua54 'on'
+fx_version 'cerulean'
+games {'gta5'}
+
+-- Resource stuff
+name 'PingStrike'
+description 'A simple FiveM resource that check player pings every so often and assigns them strikes. After a certain amount of strikes received, the player is kicked for high ping usage!'
+version 'v1'
+author 'ReckerXF'
+
+files {
+    "Newtonsoft.Json.dll"
+}
+
+server_script 'PingStrike.Server.net.dll'
